f6553aa18bc6bf51088327384c97c74622893d67
ZulaicaE <zulaicaemiliano@gmail.com>
2024-05-03 17:59:22 -0300

    Mas cambio al intro

--- proyecto-frontend/app/components/home/loginButton/LoginButton.tsx
+++ proyecto-frontend/app/components/home/loginButton/LoginButton.tsx
@@ -1,5 +1,7 @@
+import './LoginButton.css'
+
 export const LoginButton = () => {
     return (
-        <button type="button" className="btn btn-danger">Login</button>
+        <button type="button" className="btn loginButton">Login</button>
     );
 }
