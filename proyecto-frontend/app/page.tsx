4405b4cb8fd913385dcc5be3c541bb9932d16b1a
TOConde <tomy_t.c@hotmail.com>
2024-05-06 16:07:00 -0300

    componente FaQ con estilados

--- proyecto-frontend/app/page.tsx
+++ proyecto-frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from './page.module.css';
 import { HomeIntro } from "./components/home/homeIntro/HomeIntro";
 import { Footer } from './components/footer/Footer';
+import { Faq } from './components/home/faq/Faq';
 
 export default function Home() {
   return (
     <main className={styles.main}>
         <HomeIntro />
+        <Faq />
         <Footer />
     </main>
   );
