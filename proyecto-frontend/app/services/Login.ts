b4b04d2bfec86362f70b152c6016444b3342f780
ZulaicaE <zulaicaemiliano@gmail.com>
2024-05-27 08:43:21 -0300

    Fix del login con ruteo usando token storage

--- proyecto-frontend/app/services/Login.ts
+++ proyecto-frontend/app/services/Login.ts
@@ -1,28 +1,18 @@
-import { Route } from 'next';
 import clienteAxios from './Axios';
 
-export const loginUser = async (user: any, router: any) => {
+export const loginUser = async (user: any): Promise<boolean> => {
   try {
-    const response = await fetch("http://localhost:8080/login", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    })
-
-    const json = await response.json();
-    console.log(json);
-
-    if (json.role === 'ADM') {
-      router.push('/admin');
-    } else if (json.role === 'USR') {
-      router.push('/browse');
-    } else {
-      throw new Error('Role de usuario Invalido.')
-    }
-
+    const response = await clienteAxios.post("/login", user)
+    const token = response.data.accessToken;
+    localStorage.setItem("accessToken", token);
+    return true;
   } catch (error) {
-    console.log('Error al logearse.', error)
+    return false;
   }
+}
+
+
+export const getInformacionUsuario = async (): Promise<{email: string, role: string}> => {
+  const response = await clienteAxios.get("/usuarios/info");
+  return response.data;
 }
