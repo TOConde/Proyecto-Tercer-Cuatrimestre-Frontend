61993fd89823eba1a7bff0c2e1ca91d4a737c4a3
TOConde <tomy_t.c@hotmail.com>
2024-05-30 15:59:45 -0300

    css de las tarjetas

--- proyecto-frontend/app/components/home/subscriptions/HomeSubscriptions.tsx
+++ proyecto-frontend/app/components/home/subscriptions/HomeSubscriptions.tsx
@@ -2,37 +2,10 @@ import './HomeSubscriptions.css';
 
 export const HomeSuscriptions = () => {
     return (
-        <div className="card-group">
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Basico con anuncios</h5>
-                    <p className="card-text">Solo un dispositivo a la vez.</p>
-                    <p className="card-text">Resolucion Full HD.</p>
-                    <p className="card-text">Acceso limitado en peliculas y series.</p>
-                    <p className="card-text">GRATIS</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Premium</h5>
-                    <p className="card-text">Hasta 4 dispositivos a la vez.</p>
-                    <p className="card-text">Resolucion 4k.</p>
-                    <p className="card-text">Acceso ilimitado.</p>
-                    <p className="card-text">50 descargas para disfrutar offline.</p>
-                    <p className="card-text">4.999/mes</p>
-                </div>
-            </div>
-        </div>
-    );
-};
-
-
-{/* <div className="container text-center containerSubs">
-    <div className="container text-center containerCard">
-        <div className="row">
-            <div className="col">
-                <div className="card cardBorder">
-                    <div className="card-body cardSubs">
+        <div className="container containerSubscription text-center">
+            <div className="card-group">
+                <div className="card cardContainer">
+                    <div className="card-body">
                         <h5 className="card-title">Basico con anuncios</h5>
                         <p className="card-text">Solo un dispositivo a la vez.</p>
                         <p className="card-text">Resolucion Full HD.</p>
@@ -40,10 +13,8 @@ export const HomeSuscriptions = () => {
                         <p className="card-text">GRATIS</p>
                     </div>
                 </div>
-            </div>
-            <div className="col">
-                <div className="card cardBorder">
-                    <div className="card-body cardSubs">
+                <div className="card cardContainer">
+                    <div className="card-body">
                         <h5 className="card-title">Premium</h5>
                         <p className="card-text">Hasta 4 dispositivos a la vez.</p>
                         <p className="card-text">Resolucion 4k.</p>
@@ -54,5 +25,6 @@ export const HomeSuscriptions = () => {
                 </div>
             </div>
         </div>
-    </div>
-</div> */}
+    );
+};
+       
