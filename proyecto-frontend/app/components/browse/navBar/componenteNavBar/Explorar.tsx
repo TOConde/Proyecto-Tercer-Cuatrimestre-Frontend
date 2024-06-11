e65db3cf853f620fa38ada1bd881fce649d77aaa
TOConde <tomy_t.c@hotmail.com>
2024-06-11 10:55:44 -0300

    dropdown en boton explorar

--- proyecto-frontend/app/components/browse/navBar/componenteNavBar/Explorar.tsx
+++ proyecto-frontend/app/components/browse/navBar/componenteNavBar/Explorar.tsx
@@ -1,9 +1,20 @@
+import Dropdown from 'react-bootstrap/Dropdown';
 import './Explorar.css'
 
-export const NavBarExplorar = () => {
+function NavBarExplorar() {
   return (
-    <div className='conteinerExplorar'>
-      <p className='textExplorar'>Explorar</p>
-    </div>
+    <Dropdown className='conteinerExplorar'>
+      <Dropdown.Toggle id="dropdown-basic" className='textExplorar btnExplorar'>
+        Explorar
+      </Dropdown.Toggle>
+      <Dropdown.Menu className='dropMenu'>
+        <Dropdown.Item className='dropItem' href="#/action-1">Genero 1</Dropdown.Item>
+        <Dropdown.Item className='dropItem' href="#/action-2">Genero 2</Dropdown.Item>
+        <Dropdown.Item className='dropItem' href="#/action-3">Genero 3</Dropdown.Item>
+      </Dropdown.Menu>      
+    </Dropdown>
+    
   );
 }
+
+export default NavBarExplorar;
