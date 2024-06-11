63d723255de6dc1b55d6a22bb34d4a195961ca2f
TOConde <tomy_t.c@hotmail.com>
2024-06-11 13:57:42 -0300

    componente browseintro

--- proyecto-frontend/app/(routes)/browse/page.tsx
+++ proyecto-frontend/app/(routes)/browse/page.tsx
@@ -3,11 +3,15 @@
 import { withRoles } from '@/app/components/HOC/WithRoles';
 import { NavBarBrowse } from '../../components/browse/navBar/NavBar';
 import styles from '../../page.module.css';
+import { ListaPelis } from '@/app/components/browse/listaPelis/ListaPelis';
+import { BrowseIntro } from '@/app/components/browse/intro/BrowseIntro';
 
 const Browse = () => {
   return (
     <main className={styles.main}>
       <NavBarBrowse />
+      <BrowseIntro />
+      <ListaPelis />
     </main>
   );
 }
