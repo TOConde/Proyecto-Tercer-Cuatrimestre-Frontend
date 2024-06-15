15a238c1feb6f7d5a9661fea9d00bc42175ff86b
ZulaicaE <zulaicaemiliano@gmail.com>
2024-06-15 15:29:06 -0300

    Redireccion de pagina con token

--- proyecto-frontend/app/page.tsx
+++ proyecto-frontend/app/page.tsx
@@ -5,14 +5,40 @@ import { HomeIntro } from "./components/home/homeIntro/HomeIntro";
 import { HomeAbout } from './components/home/homeAbout/HomeAbout';
 import { HomeSuscriptions } from './components/home/subscriptions/HomeSubscriptions';
 import { Faq } from './components/home/faq/Faq';
+import { useEffect } from 'react';
+import { getInformacionUsuario } from './services/Login';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const redireccionarUsuarioLogeado = async () => {
+    try {
+      const userData = await getInformacionUsuario();
+      if (userData?.role === "ADM") {
+        router.push("./admin");
+      } else {
+        router.push("./browse");
+      }
+    } catch {
+      localStorage.removeItem('accessToken');
+    }
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      redireccionarUsuarioLogeado();
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
-        <HomeIntro />
-        <HomeAbout />
-        <HomeSuscriptions />
-        <Faq />        
+      <HomeIntro />
+      <HomeAbout />
+      <HomeSuscriptions />
+      <Faq />
     </main>
   );
 }
