4e7318467169fb2da3c2e5953002c0e931f9220c
ZulaicaE <zulaicaemiliano@gmail.com>
2024-06-18 17:31:24 -0300

    Browse muestra peliculas de DB

--- proyecto-frontend/app/components/browse/listaPelis/ListaPelis.tsx
+++ proyecto-frontend/app/components/browse/listaPelis/ListaPelis.tsx
@@ -1,28 +1,45 @@
+import { Button } from 'react-bootstrap';
 import './ListaPelis.css'
+import { getAllMovies } from '@/app/services/BrowsePeliculas';
+import { useEffect, useState } from 'react';
+
+interface Movie {
+  peliculaID: number;
+  titulo: string;
+  sinopsis: string;
+  url_image: string;
+  url_image_delete: string;
+}
 
 export const ListaPelis = () => {
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const allMovies = await getAllMovies();
+        setMovies(allMovies);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
-    <div className="containerListaRecomen">
-      <div>
-        <p>Lista de Recomendación</p>
-      </div>
-      <div className="row">
-        <div className="col">
-          <img className='imgMovie' src="/img/browse/movies/bandOfBrother.webp" alt="" />
+      <div className="containerListaRecomen">
+        <div>
+          <p>Lista de Recomendación</p>
         </div>
-        <div className="col">
-          <img className='imgMovie' src="/img/browse/movies/bladeRunner2049.webp" alt="" />
-        </div>
-        <div className="col">
-          <img className='imgMovie' src="/img/browse/movies/johnWick2.webp" alt="" />
-        </div>
-        <div className="col">
-          <img className='imgMovie' src="/img/browse/movies/pulFiction.webp" alt="" />
-        </div>
-        <div className="col">
-          <img className='imgMovie' src="/img/browse/movies/theTrumanShow.webp" alt="" />
+        <div className="row">
+          {movies.map((movie, index) => (
+            <div className="col" key={index}>
+              <img className='imgMovie' src={movie.url_image} />
+            </div>
+          ))}
         </div>
       </div>
-    </div>
   );
 }
