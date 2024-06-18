678acef02f08ea9f2a61b7ec4424ce44f41b93a0
TOConde <tomy_t.c@hotmail.com>
2024-06-18 14:40:39 -0300

    estilado admin

--- proyecto-frontend/app/(routes)/admin/page.tsx
+++ proyecto-frontend/app/(routes)/admin/page.tsx
@@ -1,12 +1,12 @@
 'use client'
-
+import styles from './page.module.css';
 import { withRoles } from '@/app/components/HOC/WithRoles'
 import { FormAdmin } from '@/app/components/admin/form/FormAdmin'
 import { TituloAdmin } from '@/app/components/admin/titulo/Titulo'
 
 const Page = () => {
     return (
-        <div>
+        <div className={styles.main}>
             <TituloAdmin />        
             <FormAdmin />
         </div>
--- proyecto-frontend/app/components/admin/form/FormAdmin.tsx
+++ proyecto-frontend/app/components/admin/form/FormAdmin.tsx
@@ -4,10 +4,10 @@ import { useState } from 'react';
 import { agregarPelicula } from '@/app/services/AgregarPelicula';
 
 export const FormAdmin = () => {
-
   const [titulo, setTitulo] = useState('');
   const [sinopsis, setSinopsis] = useState('');
   const [img, setImg] = useState<File | null>(null);
+  const [fileName, setFileName] = useState('Formato .jpg/.jpeg/.png');
 
   const handleChangeTitulo = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitulo(e.target.value);
@@ -20,6 +20,7 @@ export const FormAdmin = () => {
   const handleChangeImg = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       setImg(e.target.files[0]);
+      setFileName(e.target.files[0].name)
     }
   };
 
@@ -39,6 +40,7 @@ export const FormAdmin = () => {
     if (agregadoExitoso) {
       setTitulo('');
       setSinopsis('');
+      setFileName('Formato .jpg/.jpeg/.png');
       console.log('Película cargada con éxito.');
     } else {
       console.log('Error al cargar la película.');
@@ -53,9 +55,11 @@ export const FormAdmin = () => {
       <FloatingLabel controlId="floatingSinopsis" label="Sinopsis" className="formAdmLabel">
         <Form.Control type="text" placeholder="Sinopsis" className="containerFormInput" value={sinopsis} onChange={handleChangeSinopsis} />
       </FloatingLabel>
-      <FloatingLabel controlId="floatingThumbnail" label="Thumbnail" className="formAdmLabel">
-        <Form.Control type="file" placeholder="Thumbnail" className="containerFormInput" onChange={handleChangeImg} />
-      </FloatingLabel>
+      <div className="formAdmLabel">
+        <label htmlFor="file" className="fileInputLabel">Seleccionar imagen...</label>
+        <input type="file" id="file" className="fileInput" onChange={handleChangeImg} />
+        {fileName && <span className="fileName">{fileName}</span>}
+      </div>
       <Button type="submit" className='bttnAgregar'>Agregar</Button>
     </form>
   );
