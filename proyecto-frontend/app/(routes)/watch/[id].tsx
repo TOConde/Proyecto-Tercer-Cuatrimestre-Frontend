10bcb58e4a2912423a9c05ccb0259ac8a5496e9d
TOConde <tomy_t.c@hotmail.com>
2024-07-03 18:59:52 -0300

    routes dinamico

--- proyecto-frontend/app/(routes)/watch/[id].tsx
+++ proyecto-frontend/app/(routes)/watch/[id].tsx
@@ -1,34 +1,25 @@
-import { useEffect, useState } from 'react';
 import styles from './page.module.css';
-import { Video } from '@/app/components/watch/video/Video';
 import { getMovieById } from '@/app/services/Peliculas';
 import { useRouter } from 'next/router';
 
 const Watch = () => {
   const router = useRouter();
   const { id } = router.query;
-  const [videoUrl, setVideoUrl] = useState<string>('');
 
-  useEffect(() => {
-    const fetchMovieDetails = async () => {
-      try {
-        const movieDetails = await getMovieById(Number(id));
-        if (movieDetails && movieDetails.urlVideo) {
-          setVideoUrl(movieDetails.urlVideo);
-        } else {
-          console.error(`No se encontró el video para la película con ID ${id}`);
-        }
-      } catch (error) {
-        console.error('Error fetching movie details:', error);
-      }
-    };
+  const fetchMovieDetails = async () => {
+    try {
+      const movieDetails = await getMovieById(Number(id));
+      console.log(movieDetails)
+    } catch (error) {
+      console.log('Error fetching movie details:', error);
+    }
+  }
 
-    fetchMovieDetails();
-  }, [id]);
+  fetchMovieDetails();
 
   return (
     <div className={styles.main}>
-      {<Video src={videoUrl} />}
+      <h1>watch {id}</h1>
     </div>
   );
 };
