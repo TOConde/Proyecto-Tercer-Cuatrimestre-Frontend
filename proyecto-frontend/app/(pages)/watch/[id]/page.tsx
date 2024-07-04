1d4154ec10bee88d9f511d04d5942650bb0023aa
TOConde <tomy_t.c@hotmail.com>
2024-07-04 10:42:09 -0300

    pagina watch routeo dinamico pasa el video como prop

--- proyecto-frontend/app/(pages)/watch/[id]/page.tsx
+++ proyecto-frontend/app/(pages)/watch/[id]/page.tsx
@@ -1,28 +1,41 @@
 'use client'
+import { Video } from '@/app/components/watch/Video';
 import styles from './page.module.css';
 import { getMovieById } from '@/app/services/Peliculas';
 import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
+interface Movie {
+  id: number;
+  title: string;
+  urlVideo: string;
+}
 
 const Peliculas = () => {
   const params = useParams<{ id: string }>()
   const { id } = params;
+  const [movie, setMovie] = useState<Movie | null>(null);
 
-  /* const fetchMovieDetails = async () => {
-    try {
-      const movieDetails = await getMovieById(Number(id));
-      console.log(movieDetails)
-    } catch (error) {
-      console.log('Error fetching movie details:', error);
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const movieDetails = await getMovieById(Number(id));
+        setMovie(movieDetails)
+      } catch (error) {
+        console.log('Error fetching movie details:', error);
+      }
     }
+
+    fetchMovieDetails();
+  }, [id])
+
+  if (!movie) {
+    return <div className={styles.loading}>Pelicula no encontrada</div>;
   }
-  fetchMovieDetails();
- */
-  console.log(params)
 
   return (
     <div className={styles.main}>
-      <h1>watch {id}</h1>
+      <Video src={movie.urlVideo} />
     </div>
   );
 };
