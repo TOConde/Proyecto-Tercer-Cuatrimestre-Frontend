429ef1e5a24a39494c0da7839f2a2324a49f1b77
TOConde <tomy_t.c@hotmail.com>
2024-07-05 15:44:45 -0300

    raiting, con numeros al azar

--- proyecto-frontend/app/components/watch/infoMovie/InfoMovie.tsx
+++ proyecto-frontend/app/components/watch/infoMovie/InfoMovie.tsx
@@ -1,4 +1,5 @@
 import { ImagenWatch } from './componentesInfoMovie/poster/ImagenWatch';
+import { RatingWatch } from './componentesInfoMovie/raiting/RatingWatch';
 import { SinopsisWatch } from './componentesInfoMovie/sinopsis/SinopsisWatch';
 import { TituloWatch } from './componentesInfoMovie/titulo/TituloWatch';
 import './InfoMovie.css';
@@ -13,11 +14,13 @@ interface InfoMovieProps {
 
 export const InfoMovie = ({ movieInfo }: InfoMovieProps) => {
   const { titulo, sinopsis, url_image } = movieInfo;
+  const ratingNumber: number = Math.round((Math.random() * 5) * 10 ) / 10; //numero randmo 0 a 5
 
   return (
     <div className='infoMoviContainer'>
       <div className='infoTituloSinopContainer'>
         <TituloWatch titulo={titulo} />
+        <RatingWatch ratingNumber={ratingNumber} />
         <SinopsisWatch sinopsis={sinopsis} />
       </div>
       <div className='infoImagenConteiner'>
