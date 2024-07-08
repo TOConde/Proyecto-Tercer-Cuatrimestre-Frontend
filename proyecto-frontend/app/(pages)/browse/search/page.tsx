8f12d9625cb454040f10b32cef53c99ebc626914
TOConde <tomy_t.c@hotmail.com>
2024-07-08 16:14:47 -0300

    saque un console log de watch

--- proyecto-frontend/app/(pages)/browse/search/page.tsx
+++ proyecto-frontend/app/(pages)/browse/search/page.tsx
@@ -1,14 +1,22 @@
 'use client'
 
+import { useSearchParams } from 'next/navigation';
 import { withRoles } from '@/app/components/HOC/WithRoles';
 import styles from '../page.module.css';
 import { NavBarBrowse } from '@/app/components/browse/navBar/navBar';
 
 
 const Search = () => {
+  const params = useSearchParams();
+
+  const searchQuery = params.get('query') || '';
+ 
+
   return (
     <main className={styles.main}>
-      <NavBarBrowse />
+      <NavBarBrowse /><br />
+      <h1>Para la busqueda: "{params}"</h1>
+      <h1>Encontramos las siguientes coincidencias</h1>
     </main>
   );
 }
