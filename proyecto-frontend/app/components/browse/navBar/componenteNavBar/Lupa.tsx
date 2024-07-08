de0eb9ede2f3980c05f1c691a8f2c6d1d4c4253c
TOConde <tomy_t.c@hotmail.com>
2024-07-08 15:35:27 -0300

    manejo de logica lupa, lleva a /search con el query

--- proyecto-frontend/app/components/browse/navBar/componenteNavBar/Lupa.tsx
+++ proyecto-frontend/app/components/browse/navBar/componenteNavBar/Lupa.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import './Lupa.css'
 import { IoSearchSharp } from "react-icons/io5";
 
 export const NavBarLupa = () => {
+  const [inputVisible, setInputVisible] = useState(false);
+  const [searchWord, setSearchWord] = useState('');
+
+  const handleSearchClick = () => {
+    setInputVisible(!inputVisible);
+  }
+
+  const handleSearchChange = (e: any) => {
+    setSearchWord(e.target.value);
+  }
+
+  const handleSearchSubmit = (e: any) => {
+    e.preventDefault();
+    if (searchWord.trim() !== '') {
+      window.location.href = `/browse/search?queary${searchWord}`
+    }
+  }
+
   return (
     <div className='containerLupa'>
-      <IoSearchSharp className='lupaImg'/>
+      <IoSearchSharp className='lupaImg' onClick={handleSearchClick}/>
+      {inputVisible && (
+        <form onSubmit={handleSearchSubmit} className='searchForm'>
+          <input
+           type="text"
+           value={searchWord}
+           onChange={handleSearchChange}
+           placeholder='Busque una pelicula...'
+           className='searchInput'
+          />
+        </form>
+      )}
     </div>
   );
 }
