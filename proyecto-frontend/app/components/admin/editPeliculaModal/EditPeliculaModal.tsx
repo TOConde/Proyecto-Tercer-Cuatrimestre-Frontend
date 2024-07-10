6626759e35178e3a9a93884e3a1daeaec539b3f3
TOConde <tomy_t.c@hotmail.com>
2024-07-10 18:17:46 -0300

    validar que minimo un genero este seleccionado

--- proyecto-frontend/app/components/admin/editPeliculaModal/EditPeliculaModal.tsx
+++ proyecto-frontend/app/components/admin/editPeliculaModal/EditPeliculaModal.tsx
@@ -1,8 +1,8 @@
 import './EditPeliculaModal.css';
-import { Button, Form, Modal, Tab, Tabs } from "react-bootstrap";
+import { Button, Col, Form, Modal, Row, Tab, Tabs } from "react-bootstrap";
 import { Pelicula } from "../tablaPelicula/TablaPelicula";
 import { useEffect, useState } from 'react';
-import { editMovie } from '@/app/services/Peliculas';
+import { editMovie, getAllGeneros, getGenerosById } from '@/app/services/Peliculas';
 
 
 interface EditPeliculaModalProps {
@@ -14,15 +14,49 @@ interface EditPeliculaModalProps {
 
 const EditPeliculaModal: React.FC<EditPeliculaModalProps> = ({ show, handleClose, pelicula, actualizarPeliculas }) => {
   const [updatedPelicula, setUpdatedPelicula] = useState<Pelicula>(pelicula);
+  const [generos, setGeneros] = useState([]);
+  const [selectedGeneros, setSelectedGeneros] = useState<number[]>([]);
 
   useEffect(() => {
     setUpdatedPelicula(pelicula);
+  }, [pelicula]);
+
+  useEffect(() => {
+    const fetchGeneros = async () => {
+      try {
+        const allGeneros = await getAllGeneros();
+        setGeneros(allGeneros);
+
+        const peliculaGeneros = await getGenerosById(pelicula.peliculaID);
+        setSelectedGeneros(peliculaGeneros.map((g: any) => g.generoID));
+      } catch (error) {
+        console.error('Error fetching generos:', error);
+      }
+    };
+
+    if (pelicula && pelicula.peliculaID) {
+      fetchGeneros();
+    }
   }, [pelicula]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setUpdatedPelicula({ ...updatedPelicula, [name]: value });
   }
+
+  const handleSelectGenero = (generoID: number) => {
+    setSelectedGeneros((prev) => {
+      if (prev.includes(generoID)) {
+        if (prev.length > 1) {
+          return prev.filter(id => id !== generoID);
+        } else {
+          return prev;
+        }
+      } else {
+        return [...prev, generoID];
+      }
+    });
+  };
 
   const handleSaveChange = async () => {
     try {
@@ -71,7 +105,21 @@ const EditPeliculaModal: React.FC<EditPeliculaModalProps> = ({ show, handleClose
             </Form>
           </Tab>
           <Tab eventKey="Generos" title="Generos">
-            generos
+            <Form.Group controlId="formGeneros" className='formAdmLabel containerGenero'>
+              <Row>
+                {generos.map((genero: any) => (
+                  <Col key={genero.generoID} xs={6}>
+                    <Form.Check
+                      type="checkbox"
+                      id={`genero-${genero.generoID}`}
+                      label={genero.nombreGenero}
+                      onChange={() => handleSelectGenero(genero.generoID)}
+                      checked={selectedGeneros.includes(genero.generoID)}
+                    />
+                  </Col>
+                ))}
+              </Row>
+            </Form.Group>
           </Tab>
         </Tabs>
       </Modal.Body>
