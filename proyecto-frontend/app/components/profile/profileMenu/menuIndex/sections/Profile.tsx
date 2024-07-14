9789fc2ab70ffd975639e6fd211c0e1aef0f50e0
ZulaicaE <zulaicaemiliano@gmail.com>
2024-07-14 04:40:23 -0300

    validaciones home y perfil utilizando react boostrap

--- proyecto-frontend/app/components/profile/profileMenu/menuIndex/sections/Profile.tsx
+++ proyecto-frontend/app/components/profile/profileMenu/menuIndex/sections/Profile.tsx
@@ -6,11 +6,13 @@ import './Profile.css';
 import { editUserProfile, getUserById } from '@/app/services/User';
 
 function Profile() {
-
     const [nombre, setNombre] = useState<string>('');
     const [edad, setEdad] = useState<number>(0);
     const [pais, setPais] = useState<string>('');
     const [fechaSuscripcion, setFechaSuscripcion] = useState<string>('');
+    const [validated, setValidated] = useState<boolean>(false);
+    const [nombreValid, setNombreValid] = useState<boolean>(true);
+    const [edadValid, setEdadValid] = useState<boolean>(true);
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -30,15 +32,18 @@ function Profile() {
         };
 
         fetchUserData();
-
     }, []);
 
     const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
         setNombre(e.target.value);
+        const nombreRegex = /^[a-zA-Z\s]{1,30}$/;
+        setNombreValid(nombreRegex.test(e.target.value));
     };
 
     const handleChangeEdad = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEdad(parseInt(e.target.value, 10));
+        const value = parseInt(e.target.value, 10);
+        setEdad(value);
+        setEdadValid(value >= 18);
     };
 
     const handleChangePais = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -47,32 +52,61 @@ function Profile() {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const body = {
-            nombre,
-            edad,
-            pais
+        const form = e.currentTarget;
+        if (form.checkValidity() === false || !nombreValid || !edadValid) {
+            e.stopPropagation();
+        } else {
+            const body = {
+                nombre,
+                edad,
+                pais
+            };
+
+            try {
+                await editUserProfile(body);
+                console.log('Perfil Editado.');
+            } catch (error) {
+                console.log('No se pudo editar el perfil.', error);
+            }
         }
 
-        try {
-            await editUserProfile(body);
-            console.log('Perfil Editado.');
-        } catch (error) {
-            console.log('No se pudo editar el perfil.', error);
-        }
-    }
+        setValidated(true);
+    };
 
     return (
         <Card className="profile-card">
             <Card.Body className="profile-card-body">
                 <Card.Title className="profile-card-title">Perfil</Card.Title>
-                <Form onSubmit={handleSubmit} className="profile-form">
+                <Form noValidate validated={validated} onSubmit={handleSubmit} className="profile-form">
                     <Form.Group className="mb-3 profile-form-group" controlId="formNombre">
                         <Form.Label className="form-label">Nombre</Form.Label>
-                        <Form.Control type="text" placeholder="" className="form-control" value={nombre} onChange={handleChangeName} />
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            className="form-control"
+                            value={nombre}
+                            onChange={handleChangeName}
+                            isInvalid={!nombreValid}
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            No utilizar números o símbolos y máximo 30 caracteres.
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group className="mb-3 profile-form-group" controlId="formEdad">
                         <Form.Label className="form-label">Edad</Form.Label>
-                        <Form.Control type="number" placeholder="" className="form-control" value={edad} onChange={handleChangeEdad} />
+                        <Form.Control
+                            type="number"
+                            placeholder=""
+                            className="form-control"
+                            value={edad}
+                            onChange={handleChangeEdad}
+                            isInvalid={!edadValid}
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Debe ser mayor de edad.
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="formPais" className="profile-form-group">
                         <Form.Label className="form-label">País</Form.Label>
@@ -90,7 +124,13 @@ function Profile() {
                     </Form.Group>
                     <Form.Group className="mb-3 profile-form-group" controlId="formFechaSuscripcion">
                         <Form.Label className="form-label">Fecha de Suscripción</Form.Label>
-                        <Form.Control disabled type="date" placeholder="" className="form-control" value={fechaSuscripcion} />
+                        <Form.Control
+                            disabled
+                            type="date"
+                            placeholder=""
+                            className="form-control"
+                            value={fechaSuscripcion}
+                        />
                     </Form.Group>
                     <Button variant="primary" type="submit" className="profile-button">
                         Editar
